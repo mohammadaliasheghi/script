2ec7404ae79e00518d95f2170c665edd22200fd3
dev <mohammad.ali.asheghi@gmail.com>
2024-09-03 22:38:46 +0330

    change kafka version

--- docker/kafka.sh
+++ docker/kafka.sh
@@ -1,8 +1,10 @@
-#download kafka with docker from github
+# download kafka with docker from github
 https://github.com/conduktor/kafka-stack-docker-compose
-#start kafka with docker compose
+# start kafka with docker compose
 sudo docker-compose -f zk-single-kafka-single.yml up -d
-#must be check up (kafka1, zoo1) & docker exec command
+# must be check up (kafka1, zoo1) & docker exec command
 sudo docker exec -it kafka1 /bin/bash
-#down kafka server
+# down kafka server
 sudo docker-compose -f zk-single-kafka-single.yml down
+# check version
+kafka-topics --version
