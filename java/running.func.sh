b3dfa5cb202b24f227799126fb9a4da158fc3f59
mohammad <75945646+mohammadaliasheghi@users.noreply.github.com>
2025-01-10 16:36:50 +0330

    Update running.func.sh

--- java/running.func.sh
+++ java/running.func.sh
@@ -11,17 +11,14 @@ fi
 ejs(){
   local app_name=${1:?err! no parameter!}
   local jar_path=${2:?err! no parameter!}
-  local jar_file=${3:?err! no parameter!}
 
   if [ -d "$jar_path" ]; then
-    # shellcheck disable=SC2006
     green_bg=`tput setab 2`
-    # shellcheck disable=SC2006
     reset=`tput sgr0`
     echo "${green_bg}running $app_name${reset}"
-      # shellcheck disable=SC2164
       cd "$jar_path"
-      java -jar "$jar_file" &
+      mv *.jar app.jar
+      java -jar app.jar &
       process_id=$!
       echo $process_id
       echo "#$app_name" >> $stop_file
@@ -34,10 +31,10 @@ ejs(){
   fi
 }
 
-ejs eureka-server ~/workspace/backend/cloud-platform/eureka-server/target/ eureka-server-1.0.0.jar
-ejs cache-service ~/workspace/backend/cloud-platform/cache-service/target/ cache-service-1.0.0.jar
-ejs oauth-server ~/workspace/backend/cloud-platform/oauth-server/target/ oauth-server-0.0.1-SNAPSHOT.jar
-ejs user-service ~/workspace/backend/cloud-platform/user-service/target/ user-service-0.0.1-SNAPSHOT.jar
-ejs gateway ~/workspace/backend/cloud-platform/gateway/target/ gateway-1.0.0.jar
-ejs general-service ~/workspace/backend/generalServiceBackend/target/ general-service-0.0.1-SNAPSHOT.jar
+ejs eureka-server ~/workspace/backend/cloud-platform/eureka-server/target/
+ejs cache-service ~/workspace/backend/cloud-platform/cache-service/target/
+ejs oauth-server ~/workspace/backend/cloud-platform/oauth-server/target/
+ejs user-service ~/workspace/backend/cloud-platform/user-service/target/
+ejs gateway ~/workspace/backend/cloud-platform/gateway/target/
+ejs general-service ~/workspace/backend/generalServiceBackend/target/
 echo "all server is up."
